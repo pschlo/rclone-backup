ef6c741ae736b2a76e2e28245553b2a0da925ca1
pschlo <>
2023-07-01 16:53:32 +0200

    made serve-custom-mount more detailed

--- serve-custom-mount.sh
+++ serve-custom-mount.sh
@@ -5,6 +5,16 @@ if (($# < 2)); then echo "ERROR: Expected at least 2 positional arguments, but $
 SOURCE_PATH="$1"  # path on source, e.g. server
 shift
 
+# check if -- is contained in args
 found="false"
-for i in "$@"; do [[ "$i" == "--" ]] && found="true"; done
-if [[ $found == "false" ]]; then set -- "--" "$@"; fi
+for i in "$@"; do
+    if [[ "$i" == "--" ]]; then
+        found="true"
+        break
+    fi
+done
+
+# add -- if not contained
+if [[ $found == "false" ]]; then
+    set -- "--" "$@"
+fi
