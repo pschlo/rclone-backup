52cde613c9c9a7168e033c5d80a2975080057df0
pschlo <>
2023-07-02 00:05:51 +0200

    made variables local + shorter

--- mount-utils.sh
+++ mount-utils.sh
@@ -13,32 +13,34 @@ timestamp_ms () {
     echo $(($(date +%s%N)/1000000))
 }
 
+# $1: mount PID
 is_alive() {
-    ps -p $MOUNT_PID >/dev/null
+    ps -p "$1" >/dev/null
 }
 
+# $1: mount path
 is_mounted() {
-    mountpoint -q "$MOUNT_PATH"
+    mountpoint -q "$1"
 }
 
 
 
 
 stop_mount () {
-    MOUNT_PID=$1
-    MOUNT_PATH="$2"
-    mount_str="${MOUNT_PID}@$(basename "$MOUNT_PATH")"
+    local PID=$1
+    local POINT="$2"
+    local mount_str="${PID}@$(basename "$POINT")"
 
     if [[ ${3+x} ]]; then
-        TIMEOUT_SECS="$3"
+        local TIMEOUT_SECS="$3"
     else
-        TIMEOUT_SECS=10
+        local TIMEOUT_SECS=10
     fi
 
-    if ! is_alive; then return 0; fi
+    if ! is_alive $PID; then return 0; fi
 
-    if is_mounted; then
-        umount "$MOUNT_PATH" 2>/dev/null && true
+    if is_mounted $POINT; then
+        umount "$POINT" 2>/dev/null && true
         if (($? > 0)); then
             log_warn "unmounting $mount_str failed; killing mount process"
             # we could *wait* for processes to finish their business with the mount dir,
@@ -47,43 +49,56 @@ stop_mount () {
             # then finishes itself. Thus, the process is dead already.
 
             # kill might fail if mount process died in the meantime
-            kill $MOUNT_PID 2>/dev/null && true
+            kill $PID 2>/dev/null && true
         fi
     else
         # not yet mounted
         # kill might fail if mount process died in the meantime
-        kill $MOUNT_PID 2>/dev/null && true
+        kill $PID 2>/dev/null && true
     fi
 
     log_info "waiting for mount $mount_str to stop"
     # wait for TIMEOUT_MS millisecods for the mount process to terminate
-    TIMEOUT_MS=$((TIMEOUT_SECS*SECONDS))
-    t0=$(timestamp_ms)
+    local TIMEOUT_MS=$((TIMEOUT_SECS*SECONDS))
+    local t0=$(timestamp_ms)
     is_timeout() { (($(timestamp_ms)-t0 >= TIMEOUT_MS)); }
 
-    while is_alive && ! is_timeout; do sleep 0.1; done
-    if is_alive; then log_err "could not terminate mount process $mount_str"; return 1; fi
+    while is_alive $PID && ! is_timeout; do
+        sleep 0.1
+    done
+    if is_alive $PID; then
+        log_err "could not terminate mount process $mount_str"
+        return 1
+    fi
     log_info "mount $mount_str stopped"
     return 0
 }
 
 wait_mount () {
-    MOUNT_PID=$1
-    MOUNT_PATH="$2"
-    mount_str="${MOUNT_PID}@$(basename "$MOUNT_PATH")"
+    local PID=$1
+    local POINT="$2"
+    local mount_str="${PID}@$(basename "$POINT")"
 
     if [[ ${3+x} ]]; then
-        TIMEOUT_SECS="$3"
+        local TIMEOUT_SECS="$3"
     else
-        TIMEOUT_SECS=10
+        local TIMEOUT_SECS=10
     fi
 
-    TIMEOUT_MS=$((TIMEOUT_SECS*SECONDS))
-    t0=$(timestamp_ms)
+    local TIMEOUT_MS=$((TIMEOUT_SECS*SECONDS))
+    local t0=$(timestamp_ms)
     is_timeout() { (($(timestamp_ms)-t0 >= TIMEOUT_MS)); }
 
-    while ! is_mounted && is_alive && ! is_timeout; do sleep 0.1; done
-    if ! is_alive; then log_err "mount $mount_str stopped"; return 1; fi
-    if is_timeout && ! is_mounted; then log_err "mount $mount_str timed out"; return 1; fi
+    while ! is_mounted $POINT && is_alive $PID && ! is_timeout; do
+        sleep 0.1
+    done
+    if ! is_alive $PID; then
+        log_err "mount $mount_str stopped"
+        return 1
+    fi
+    if is_timeout && ! is_mounted $POINT; then
+        log_err "mount $mount_str timed out"
+        return 1
+    fi
     return 0
 }
